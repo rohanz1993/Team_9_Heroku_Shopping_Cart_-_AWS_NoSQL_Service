1d76a4918ba573f8c41bc280b1621f9889c2d4bb
ritika04 <ritikashetty543@gmail.com>
2016-04-20 15:53:21 -0700

    CouchDB with node.js on AWS EC2 instance

--- Final_Project/MongoLoginApp/routes/couchDB.js
+++ Final_Project/MongoLoginApp/routes/couchDB.js
@@ -1,17 +1,18 @@
 
 var http = require ('http');
-var nano = require('nano')('http://localhost:5984');
+var nano = require('nano')('http://52.37.129.148:5984');
+//IP of EC2 instance:5984
 
-nano.db.create('alice', function() {
+nano.db.create('test', function() {
     // specify the database we are going to use
-    var alice = nano.use('alice');
+    var test = nano.use('test');
     // and insert a document in it
-    alice.insert({ crazy: true }, 'rabbit', function(err, body, header) {
+    test.insert({ name: "ritika" }, 'C_001', function(err, body, header) {
       if (err) {
-        console.log('[alice.insert] ', err.message);
+        console.log('[test.insert] ', err.message);
         return;
       }
-      console.log('you have inserted the rabbit.');
+      console.log('you have inserted the Record.');
       console.log(body);
     });
   });
