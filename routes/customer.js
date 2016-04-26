b0a24b57c9f3ef183b27fe793813c8a8acd573a7
ritika04 <ritikashetty543@gmail.com>
2016-04-25 20:06:16 -0700

    Update changes

--- routes/customer.js
+++ routes/customer.js
@@ -2,9 +2,37 @@ var http = require ('http');
 var nano = require('nano')('http://localhost:5984');
 
 exports.viewProfile = function(req,res){
+	console.log("viewProfile Called");
 	res.render("customerProfile");
 };
 
 exports.getProfileDetails = function(req,res){
-	
+	console.log("getProfileDetails called");
+	var test = nano.use('test');
+	var username=req.params.user_name;
+	 test.view('login', 'by_user_name',{'key': username, 'include_docs': true}, function(err, body){
+		    if(!err){
+		    	console.log("Inside view");
+		    	var rows=body.rows;
+		    	if(typeof body.rows[0] !== "undefined")
+		        {
+		    		var doc_username = body.rows[0].value.user_name;
+		    		var password = body.rows[0].value.password;
+		    		console.log("JS output: " +body.rows[0].value);
+		    		
+		    			res.status(200).json({
+		    				message : "success",
+		    				result:	body.rows[0].value
+		    			});
+		        }
+		    	else
+		    		{
+		    		console.log("Login to continue");
+		    		}
+		    }
+		    else
+		    	{
+		    	console.log("Error "+err);
+		    	}
+		});
 };
