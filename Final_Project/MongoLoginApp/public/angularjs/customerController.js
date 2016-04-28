6eafbe8c16451ba57ccf1f1eb4437baee4fa6314
ritika04 <ritikashetty543@gmail.com>
2016-04-27 17:45:11 -0700

    Controller for updateProfile

--- Final_Project/MongoLoginApp/public/angularjs/customerController.js
+++ Final_Project/MongoLoginApp/public/angularjs/customerController.js
@@ -9,56 +9,65 @@ app.controller('CustomerController', function($scope,$http,$location,$window) {
 	
 	//get the profile details when the page is loaded
 		$scope.getProfileDetails=function(){
-			$scope.user_name="ritika";
+			$scope.email="ritika";
 				
 		//pass user_name
 		$http({
 			method : "GET",
 			url : '/getProfileDetails',
 			data : {
-				"user_name" : $scope.user_name
+				"email" : $scope.email
 			}
 		}).success(function(data) {
 			console.log("in success Customer Controller: "+JSON.stringify(data));
-			console.log(data.result.user_name);
-			$scope.firstName=data.result.user_name;
-			/*$scope.lastName=data[0].lastName;
-			$scope.zipCode=data[0].zipCode;
-			$scope.city=data[0].city;
-			$scope.email=data[0].email;
-			$scope.phoneNo=data[0].phoneNo;*/
+			$scope.userName=data.result.user_name;
+			$scope.first_name=data.result.first_name;
+			$scope.last_name=data.result.last_name;
+			$scope.address=data.result.address;
+			$scope.zipcode=data.result.zipcode;
+			$scope.email=data.result.email;
+			$scope.phone_no=data.result.phone_no;
+			$scope.card_no=data.result.card_no;
+			$scope.cvv=data.result.cvv;
+			$scope.expiry=data.result.expiry;
+		}).error(function(error) {
+			$window.alert("Error: " +JSON.stringify(error));
 		});	
 		
 	};
 
-/*	$scope.updateProfile=function(firstName,lastName,city,zipCode,email,password,phoneNo){
-		var details={
-				firstName : firstName,
-				lastName : lastName,
-				city : city,
-				zipCode : zipCode,
-				phoneNo : phoneNo,
-				customerId : $window.localStorage.customerId
-		};
-		console.log("details: "+ JSON.stringify(details));
-		dataService.postData('updateRider',details,function(err,res){
-			if(err){
-				if(err.status === 500){
-					console.log(err);
-				}else{
-					if(err.status === 403){
-						//$scope.invalidError=true;
-						console.log(err);
-					}
-				}
-			}else{
-				console.log("Success update");
-				$scope.updatesuccessmessage=true;
+	$scope.updateProfile=function(first_name,last_name,address,zipcode,email,password,phone_no,card_no,cvv,expiry){
+	
+		console.log("In updateProfile controller");
+		$http({
+			method : "POST",
+			url : '/editProfile',
+			data : {
+				"user_name" : $scope.user_name,
+				"first_name" : first_name,
+				"last_name" : last_name,
+				"address" : address,
+				"zipcode" : zipcode,
+				"email":email,
+				"password":password,
+				"phone_no" : phone_no,
+				"card_no": card_no,
+				"cvv": cvv,
+				"expiry":expiry
 			}
-		});
+		}).success(function(data) {
+			
+			console.log("in success UpdateProfile Controller: "+JSON.stringify(data));			
+			
+			$scope.updatesuccessmessage=true;
+			
+		}).error(function(error) {
+			$window.alert("Error" +JSON.stringify(error));
+		});	
+		
 	};
 
-	
+	/*	
 $scope.viewOrderHistory=function(req,res){
     	$scope.showallbillstable="true";
     	console.log("In getAllbills controller");
