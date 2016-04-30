1d54601bb7291261be72c72716b5355667577ad8
SainaPatel <saina.patel@sjsu.edu>
2016-04-30 03:21:59 -0700

    Heroku Testing for different api's

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,6 @@ exports.index = function(req, res){
 //<<<<<<< HEAD
   //res.render('search_book', { title: 'Express' });
 //=======
-  res.render('view_categories', { title: 'Express' });
+  res.render('viewCart', { title: 'Express' });
 //>>>>>>> origin/master
 };
